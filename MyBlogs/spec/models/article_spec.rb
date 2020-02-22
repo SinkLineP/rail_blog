578c37037f4b145a9150913cb5e54e46faac44ff
SinkLineP <popovartem505@gmail.com>
2020-02-22 20:08:33 +0200

    add validation article model

---
+++ MyBlogs/spec/models/article_spec.rb
@@ -0,0 +1,13 @@
+
+require 'rails_helper'
+
+describe Article do
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :text }
+  end
+
+  describe "assotiations" do
+    it { should have_many :comments }
+  end
+end
