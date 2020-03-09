87b078ba83ea970bfe5feab9caa6aa69043b6bdc
SinkLineP <popovartem505@gmail.com>
2020-03-09 17:43:54 +0200

    lesson 47 time - 54:21

--- MyBlogs/spec/features/visitor_creates_contact_spec.rb
+++ MyBlogs/spec/features/visitor_creates_contact_spec.rb
@@ -5,4 +5,14 @@ feature "Contact creation" do
     visit '/contacts'
     expect(page).to have_content I18n.t('contacts.contact_us')
   end
+
+  scenario "allows a guest to create contact" do
+    visit '/contacts'
+
+    fill_in :contact_email, :with => 'user@example.com'
+    fill_in :contact_message, :with => 'qwerty'
+    
+    click_button 'Send message'
+  end
+    
 end
