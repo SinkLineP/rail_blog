5e2a6f7dd345c8a858db6a119567fda8e31a0f16
SinkLineP <popovartem505@gmail.com>
2020-03-12 22:27:15 +0200

    heroku test 3

--- MyBlogs/config/webpack/production.js
+++ MyBlogs/config/webpack/production.js
@@ -6,3 +6,16 @@ module.exports = environment.toWebpackConfig()
 
 warnings: true
 
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const environment = require('./environment')
+
+environment.plugins.delete('UglifyJs')
+
+environment.plugins.append(
+  'UglifyJs',
+  new UglifyJsPlugin({
+    sourceMap: true
+  })
+)
+
+module.exports = environment.toWebpackConfig()
